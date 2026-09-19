ecd6e966c56c35dfff34f9906e227a72d1f898d6
Nnanna Arua Uko <nnannauko@gmail.com>
2026-09-19 11:36:09 +0100

    my first commit

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+// Controller functions for each route.
+
+const home = (req, res) => {
+  res.send('Nnanna Arua Uko');
+};
+
+module.exports = { home };
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../controllers');
+
+router.get('/', controller.home);
+
+module.exports = router;
